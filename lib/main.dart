81d7a64168a359b94f9d3270a3456ef33dac8c14
mj <onyangomaureen95@gmail.com>
2019-02-06 23:18:55 +0300

    create a class with listtile and card

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,56 @@
 import 'package:flutter/material.dart';
 
-
-void main(){
-
+void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget{
-
-  Widget build(context){
+class App extends StatelessWidget {
+  Widget build(context) {
     return MaterialApp(
       title: 'Code sample for material card',
       theme: ThemeData(
-        primarySwatch:Colors.blue,
+        primarySwatch: Colors.blue,
       ),
       home: MyStatelessWidget(),
     );
   }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  MyStatelessWidget({Key  key}) : super(key: key);
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+leading: Icon(Icons.album),
+title:Text('The Enchanted Nightingale'),
+subtitle:Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            ),
+            ButtonTheme.bar(
+              child: ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: Text('Buy Tickets'),
+                    onPressed: (){
+
+                    },
+                  ),
+                    FlatButton(
+             child: Text('LISTEN'),
+             onPressed: (){ },
+                    ),
+                  ],
+                
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-  
+}
