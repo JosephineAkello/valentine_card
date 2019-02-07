b85887269e76a4d0d863a1a20e540827f07e534c
mj <onyangomaureen95@gmail.com>
2019-02-08 02:05:12 +0300

    create a body with a network image

--- lib/home.dart
+++ lib/home.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget{
       title: 'This is our thing',
       home:Scaffold(appBar: AppBar(
       title: Text('Hello')),
-     
+       body: Image.network('https://picsum.photos/300/300/?image=1080',
    
     ),
 
