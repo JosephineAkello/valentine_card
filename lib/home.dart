61e8e317f3a84a26788e9041fa558f0c2074512f
mj <onyangomaureen95@gmail.com>
2019-02-08 02:03:42 +0300

    create home file which is a stateless widget

---
+++ lib/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  Widget build(context){
+
+    return MaterialApp(
+      title: 'This is our thing',
+      home:Scaffold(appBar: AppBar(
+      title: Text('Hello')),
+     
+   
+    ),
+
+    );
+ 
+  
+    
+    
+  }
+}
