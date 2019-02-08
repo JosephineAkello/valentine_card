45b19857d3215e68cf5444a6cbb409accdddc80f
mj <onyangomaureen95@gmail.com>
2019-02-08 12:59:56 +0300

    Create naah screen as a stateless widget

---
+++ lib/NaahScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NaahScreen extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      title: 'This is our thing',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hello')),
+        body: Image.network(
+          'https://picsum.photos/300/300/?image=1080',
+        ),
+      ),
+    );
+  }
+}
